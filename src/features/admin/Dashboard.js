48c5d61ed125f9ac00928cec3fc57521989b976f
Jasur <jasurgofirov@gmail.com>
2022-09-17 20:24:22 +0300

    Step2 component almost done

--- src/features/admin/Dashboard.js
+++ src/features/admin/Dashboard.js
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
-import ContentComp from './content';
-import Step1 from './Step1';
+import React, { useState } from "react";
+import ContentComp from "./content";
+import Step1 from "./Step1";
+import Step2 from "./Step2";
 // import PropTypes from 'prop-types';
 
 export default function Dashboard() {
-  const [status, setstatus] = useState(0)
+  const [status, setstatus] = useState(2);
   return (
-    <div className="admin-dashboard">
-      {renderSwitch(status, setstatus)}
-    </div>
+    <div className="admin-dashboard">{renderSwitch(status, setstatus)}</div>
   );
-};
+}
 
 const renderSwitch = (status, setstatus) => {
   switch (status) {
     case 1:
-      return <Step1 />
+      return <Step1 />;
+    case 2:
+      return <Step2 />;
     default:
-      return <ContentComp setstatus={setstatus}/>
+      return <ContentComp setstatus={setstatus} />;
   }
-}
+};
 
 Dashboard.propTypes = {};
 Dashboard.defaultProps = {};
